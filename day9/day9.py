aa8a433355478f7a6961f122d2ce17b012724fcf
Michał Górski <42848968+mikegorski@users.noreply.github.com>
2022-12-09 14:30:28 +0100

    Update day9.py

--- day9/day9.py
+++ day9/day9.py
@@ -1,5 +1,5 @@
 from math import sqrt
-from numpy import unique, sign
+from numpy import sign
 
 def parse_input(filename):
     with open(filename) as f:
@@ -7,27 +7,24 @@ def parse_input(filename):
     return input
 
 def move_head(initial_position, direction):
-    position = initial_position
-    if direction == 'R':
-        position[1] += 1
-    elif direction == 'L':
-        position[1] -= 1
-    if direction == 'U':
-        position[0] += 1
-    elif direction == 'D':
-        position[0] -= 1
-    return position
+    row, col = initial_position
+    if direction == 'R': col += 1
+    elif direction == 'L': col -= 1
+    elif direction == 'U': row += 1
+    elif direction == 'D': row -= 1
+    return (row, col)
 
 def move_tail(initial_position, head_position):
     position = initial_position
+    row, col = position
     dist0 = head_position[0] - position[0]
     dist1 = head_position[1] - position[1]
     dist = sqrt(dist0 ** 2 + dist1 ** 2)
     if dist > sqrt(2):
         direction = [sign(dist0), sign(dist1)]
-        position[0] = position[0] + 1 * direction[0]
-        position[1] = position[1] + 1 * direction[1]
-    return position
+        row = position[0] + 1 * direction[0]
+        col = position[1] + 1 * direction[1]
+    return (row, col)
 
 
 if __name__ == "__main__":
@@ -35,24 +32,22 @@ if __name__ == "__main__":
     instructions = parse_input('data.txt')
 
     ### TASK 1 ###
-    visited_positions = []
-    starting_positions = [[0, 0] for _ in range(2)]
-    visited_positions.append([i for i in starting_positions[0]])
-    
+    starting_positions = [(0, 0) for _ in range(2)]
+    visited_positions = {starting_positions[0]}
+
     for direction, n_steps in instructions:
         for step in range(int(n_steps)):
             starting_positions[1] = move_head(starting_positions[1], direction)
             starting_positions[0] = move_tail(starting_positions[0],
                                              starting_positions[1])
-            visited_positions.append([i for i in starting_positions[0]])
+            visited_positions.add(starting_positions[0])
     
-    visited_positions_count = len((unique(visited_positions, axis = 0)))
+    visited_positions_count = len(visited_positions)
     print(f'In total {visited_positions_count} positions were visited.')
 
     ### TASK 2 ###
-    visited_positions = []
-    starting_positions = [[0, 0] for _ in range(10)]
-    visited_positions.append([i for i in starting_positions[0]])
+    starting_positions = [(0, 0) for _ in range(10)]
+    visited_positions = {starting_positions[0]}
     
     for direction, n_steps in instructions:
         for step in range(int(n_steps)):
@@ -60,7 +55,7 @@ if __name__ == "__main__":
             for knot in range(8, -1, -1):
                 starting_positions[knot] = move_tail(starting_positions[knot],
                                                     starting_positions[knot + 1])
-            visited_positions.append([i for i in starting_positions[0]])
+            visited_positions.add(starting_positions[0])
     
-    visited_positions_count = len((unique(visited_positions, axis = 0)))
+    visited_positions_count = len(visited_positions)
     print(f'In total {visited_positions_count} positions were visited.')
